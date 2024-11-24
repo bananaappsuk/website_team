75baa1fcf95cba511a97ea23a8efb6ccc2eea1d3
KPhaniPrasad <37287890+KPhaniPrasad@users.noreply.github.com>
2024-11-24 10:50:27 +0530

    Update signin.tsx

--- tasklearn_frontend/src/pages/signin.tsx
+++ tasklearn_frontend/src/pages/signin.tsx
@@ -191,7 +191,7 @@ const SignIn: React.FC = () => {
                                     htmlFor="identifier"
                                     className="sm:w-[50%] mb-2 sm:mb-0 text-[#646161]"
                                 >
-                                    Username or --staging-- email address
+                                    Username or email address
                                 </label>
                                 <input
                                     type="text"
