46a49cb41db721b6828bdcd310a0b8a33bbbc8ef
Ajith-Kanagasabai <ajithkanagasabai17@gmail.com>
2024-12-02 19:20:35 +0530

    Error

--- tasklearn_frontend/src/components/SidebarProfile.tsx
+++ tasklearn_frontend/src/components/SidebarProfile.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @next/next/no-img-element */
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 import { FC, useEffect, useState } from "react";
 import React from "react";
 import Link from "next/link";
@@ -20,7 +21,6 @@ interface Server {
 }
 
 interface SidebarProfileProps {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     userId: any;
     onServerSelect?: (server: { serverId: string; serverName: string; memberList: string[] }) => void;
     handleTasksClick: (arg: string) => void;
