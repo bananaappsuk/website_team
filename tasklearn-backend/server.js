1d755593bdf32312f052bee5007f01076cbc9820
Ajith-Kanagasabai <ajithkanagasabai17@gmail.com>
2024-12-02 19:50:07 +0530

    cors

--- tasklearn-backend/server.js
+++ tasklearn-backend/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://dev.taslearn.vercel.app', // Allow specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],       // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+}));
 app.use(express.json());
 
 // Connect to MongoDB
