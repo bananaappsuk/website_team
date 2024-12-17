18a30e1cf2cde6bfdb08f2551eedd915557ff617
Ajith-Kanagasabai <ajithkanagasabai17@gmail.com>
2024-12-18 00:34:47 +0530

    New

--- tasklearn-backend/server.js
+++ tasklearn-backend/server.js
@@ -51,7 +51,6 @@ app.use('/api/patientId', verifyToken, patientRoutes);
 // Root route for testing the API
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the task!!!! </h1>');
-  res.send('<h1>Welcome to the task!!!! </h1>');
 });
 
 // Error handling middleware
