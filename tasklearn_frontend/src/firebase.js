06a1a336545c27d4f876dfb5fc238efdd385d240
KPhaniPrasad <37287890+KPhaniPrasad@users.noreply.github.com>
2024-12-18 16:58:32 +0530

    Update firebase.js

--- tasklearn_frontend/src/firebase.js
+++ tasklearn_frontend/src/firebase.js
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "tasklearn-8f476",
   storageBucket: "tasklearn-8f476.appspot.com",
   messagingSenderId: "586415236942",
-  appId: "1:586415236942:web:cfa086bdf0546b3d00201f", 
+  appId: "1:586415236942:web:cfa086bdf0546b3d00201f",
+  
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
